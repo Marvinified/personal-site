25da3958ef9c98ee6ecbcf3f15cf85314ce8c5fa
Marvin Tunji-ola <marvinsmart9@gmail.com>
2020-07-06 22:45:08 +0100

    Fixed Firefox Issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'alpinejs'
 // Animations
 gsap.from("#one", { duration: 2, x: -100, ease: "bounce.out" });
 gsap.from("#two", { delay: 0, duration: 2, y: -650, ease: "bounce.out" });
-gsap.to(".scroll-left", { delay: 0, duration: 2, translateX: 30, ease: "ease.out", repeat: -1});
+gsap.to(".scroll-left", { delay: 0, duration: 2, translateX: 30, ease: "ease.out", repeat: -1 });
 
 
 
@@ -12,8 +12,7 @@ gsap.to(".scroll-left", { delay: 0, duration: 2, translateX: 30, ease: "ease.out
 let translateX = 0
 document.addEventListener("wheel", (event) => {
     event.preventDefault();
-    
-    console.log("Scrolled")
+
     const factor = 15 * Math.abs(event.deltaY)
     const app = document.querySelector('.app')
     const width = app.clientWidth - window.innerWidth;
@@ -22,6 +21,9 @@ document.addEventListener("wheel", (event) => {
         // scroll up
         translateX -= factor
         translateX = Math.abs(translateX) > Math.abs(width) ? -width : translateX
+        console.log(gsap)
+        console.log("Got")
+        console.log({ factor, translateX, width: app.clientWidth, inner: window.innerWidth })
         gsap.to('.app', { duration: 0.5, translateX })
     }
 
